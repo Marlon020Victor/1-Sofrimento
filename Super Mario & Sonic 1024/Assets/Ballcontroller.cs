69dc01c8f8383c7de9fe3e08f088182e739a4f3f
Marlon Victor Camara Alves <20221170150021@ifrn.local>
2023-12-04 14:35:31 -0300

    Fase Pre-Jogo pt.1

---
+++ Super Mario & Sonic 1024/Assets/Ballcontroller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballcontroller : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public int velocidade = 5;
+    private Vector2 direcao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out rb);
+        direcao = Random.insideUnitCircle.normalized;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bloco"))
+        {
+            Destroy(collision.gameObject);
+        }
+
+        direcao = Vector2.Reflect(direcao, collision.contacts[0].normal);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = direcao * velocidade;
+    }
+}
---
+++ Super Mario & Sonic 1024/Assets/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    private Rigidbody2D rb;
+    public int Velocidade = 10;
+    private Vector2 direcao;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       TryGetComponent(out rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        direcao = new Vector2(x,0).normalized; 
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = direcao * Velocidade;
+    }
+}
