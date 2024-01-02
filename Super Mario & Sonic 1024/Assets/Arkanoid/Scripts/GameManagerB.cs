7ad442cc4e19a2dc85a0194eb7ad9c4925948aae
Marlon Victor Camara Alves <20221170150021@ifrn.local>
2024-01-02 14:33:25 -0300

    Fase Pre-Jogo pt. 4

---
+++ Super Mario & Sonic 1024/Assets/Arkanoid/Scripts/GameManagerB.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerB : MonoBehaviour
+{
+    public static GameManagerB instance;
+    public int vidas = 2;
+    public int tijolosRestantes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
